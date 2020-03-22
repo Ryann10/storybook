ce95868f69885c86a2e973a92e6123241b8c923a
sjyang <ysj6608@gmail.com>
2020-03-22 23:32:52 +0900

    feat(tokens): add design tokens

---
+++ components/tokens/colors.ts
@@ -0,0 +1,81 @@
+
+export const blue100 = '#0f4c81';
+export const blue90 = '#265d8d';
+export const blue80 = '#3e6f9a';
+export const blue70 = '#5781a6';
+export const blue60 = '#6f93b3';
+export const blue50 = '#87a5c0';
+export const blue40 = '#9fb7cc';
+export const blue30 = '#b7c9d9';
+export const blue20 = '#cfdbe5';
+export const blue10 = '#e7edf2';
+
+export const blue = {
+  10: blue10,
+  20: blue20,
+  30: blue30,
+  40: blue40,
+  50: blue50,
+  60: blue60,
+  70: blue70,
+  80: blue80,
+  90: blue90,
+  100: blue100,
+};
+
+export const orange100 = '#ff6700';
+export const orange90 = '#ff7619';
+export const orange80 = '#ff8532';
+export const orange70 = '#ff944c';
+export const orange60 = '#ffa366';
+export const orange50 = '#ffb37f';
+export const orange40 = '#ffc299';
+export const orange30 = '#ffd1b2';
+export const orange20 = '#ffe0cc';
+export const orange10 = '#ffefe5';
+
+export const orange = {
+  10: orange10,
+  20: orange20,
+  30: orange30,
+  40: orange40,
+  50: orange50,
+  60: orange60,
+  70: orange70,
+  80: orange80,
+  90: orange90,
+  100: orange100,
+};
+
+export const red100 = '#9b2335';
+export const red90 = '#a53849';
+export const red80 = '#af4e5d';
+export const red70 = '#b96571';
+export const red60 = '#c37b85';
+export const red50 = '#cd919a';
+export const red40 = '#d7a7ae';
+export const red30 = '#e1bdc2';
+export const red20 = '#ebd3d6';
+export const red10 = '#f5e9ea';
+
+
+export const red = {
+  10: red10,
+  20: red20,
+  30: red30,
+  40: red40,
+  50: red50,
+  60: red60,
+  70: red70,
+  80: red80,
+  90: red90,
+  100: red100,
+};
+
+const Colors = {
+  blue,
+  orange,
+  red,
+};
+
+export default Colors;
---
+++ components/tokens/index.ts
@@ -0,0 +1,12 @@
+
+export { default as Colors } from './colors';
+
+export const Tokens = {
+  borderRadiusCircle: '50%',
+  borderRadiusNormal: '3px',
+  borderStyle: 'solid',
+  borderWidth: '1px',
+  alertPadding: '16px',
+  smallFontSize: 'smaller',
+  lineHeight: '1.5',
+};
